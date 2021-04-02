2b6d6eee7cd2d0f573e3e3ae47aaf64eb2924144
Kenta Sato <bicycle1885@gmail.com>
2021-04-02 11:42:46 +0900

    simple placeholders

--- src/Fmt.jl
+++ src/Fmt.jl
@@ -1,5 +1,38 @@
 module Fmt
 
-# Write your package code here.
+export @f_str, format
+
+struct Format
+    spec::String
+end
+
+function format(fmt::Format, args...)
+    buf = IOBuffer()
+    spec = fmt.spec
+    last = lastindex(spec)
+    i = k = 1
+    while i ≤ last
+        c = spec[i]
+        if c == '{'
+            if i == last || spec[i+1] ≠ '}'
+                error("unpaired placeholder '{'")
+            end
+            print(buf, args[k])
+            i += 1
+            k += 1
+        else
+            write(buf, c)
+        end
+        i = nextind(spec, i)
+    end
+    if k ≠ length(args) + 1
+        error("placeholders mismatch arguments")
+    end
+    return String(take!(buf))
+end
+
+macro f_str(s)
+    Format(s)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,13 @@
 using Fmt
 using Test
 
-@testset "Fmt.jl" begin
-    # Write your tests here.
+@testset "format" begin
+    @test format(f"") == ""
+    @test format(f"foobar") == "foobar"
+
+    @test format(f"Hello, {}!", "world") == "Hello, world!"
+    @test format(f"Hello, {}!", "世界") == "Hello, 世界!"
+
+    @test format(f"x = {}", -3) == "x = -3"
+    @test format(f"x = {}", 42) == "x = 42"
 end
