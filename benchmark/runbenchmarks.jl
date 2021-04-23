9e8dbcd752d96f75eed0da79cabba160ebc87088
Kenta Sato <bicycle1885@gmail.com>
2021-04-23 20:47:57 +0900

    add target selection

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -15,31 +15,43 @@ end
 
 N = 1_000_000
 SEED = 1234
-
-println(f"character (N = {$N:,})")
-Random.seed!(SEED)
-x = rand('a':'z', N)
-for fmt in [f"{}", f"{:<9}", f"{:^9}", f"{:>9}"]
-    benchmark(fmt, x)
+TARGETS = ["character", "string", "integer", "float"]
+if !isempty(ARGS)
+    TARGETS = ARGS
 end
 
-println(f"string (N = {$N:,})")
-Random.seed!(SEED)
-x = [randstring(rand(1:20)) for _ in 1:N]
-for fmt in [f"{}", f"{:s}", f"{:>20}", f"{:^20}", f"{:<20}"]
-    benchmark(fmt, x)
+if "character" in TARGETS
+    println(f"character (N = {$N:,})")
+    Random.seed!(SEED)
+    x = rand('a':'z', N)
+    for fmt in [f"{}", f"{:<9}", f"{:^9}", f"{:>9}"]
+        benchmark(fmt, x)
+    end
 end
 
-println(f"integer (N = {$N:,})")
-Random.seed!(SEED)
-x = rand(-1000:1000, N)
-for fmt in [f"{}", f"{:b}", f"{:o}", f"{:d}", f"{:x}"]
-    benchmark(fmt, x)
+if "string" in TARGETS
+    println(f"string (N = {$N:,})")
+    Random.seed!(SEED)
+    x = [randstring(rand(1:20)) for _ in 1:N]
+    for fmt in [f"{}", f"{:s}", f"{:>20}", f"{:^20}", f"{:<20}"]
+        benchmark(fmt, x)
+    end
 end
 
-println(f"float (N = {$N:,})")
-Random.seed!(SEED)
-x = randn(N)
-for fmt in [f"{}", f"{:f}", f"{:e}", f"{:g}", f"{:a}"]
-    benchmark(fmt, x)
+if "integer" in TARGETS
+    println(f"integer (N = {$N:,})")
+    Random.seed!(SEED)
+    x = rand(-1000:1000, N)
+    for fmt in [f"{}", f"{:b}", f"{:o}", f"{:d}", f"{:x}"]
+        benchmark(fmt, x)
+    end
+end
+
+if "float" in TARGETS
+    println(f"float (N = {$N:,})")
+    Random.seed!(SEED)
+    x = randn(N)
+    for fmt in [f"{}", f"{:f}", f"{:e}", f"{:g}", f"{:a}"]
+        benchmark(fmt, x)
+    end
 end
